5b69d9eca18c5d0e91d817ed6386fc9f8b8fb956
Chump <relfen@gmail.com>
2012-12-21 14:06:02 -0800

    Created base visual studio project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Genesis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
